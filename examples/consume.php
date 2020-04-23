b5b4be30ea316f15eda8c7fdf4f70c490c85093c
Matthieu Bessat <spamfree@matthieubessat.fr>
2020-04-23 10:40:07 +0200

    refactor: better consume example

--- examples/consume.php
+++ examples/consume.php
@@ -1,18 +1,28 @@
 <?php
 
+use Lefuturiste\Jobatator\Client;
+use Lefuturiste\Jobatator\ConnectionException;
+
 require '../vendor/autoload.php';
 
-$client = new \Lefuturiste\Jobatator\Client("localhost", "8962", "root", "root", "staileu");
-var_dump($client->createConnexion());
+$client = new Client("localhost", "8962", "root", "root", "staileu");
+try {
+    $client->createConnexion();
+} catch (ConnectionException $e) {
+    fwrite(STDERR, "ERR: Can't connect to jobatator server: " . $e->getMessage() . PHP_EOL);
+    exit(1);
+}
 
 $client->setRootValue("Hello");
 $client->addHandler("job.type1", function(array $payload, $rootValue) {
     var_dump($payload);
     var_dump($rootValue);
+    return true; // return true if the job was successful and false if not
 });
 
 $client->addHandler("job.type2", function(array $payload) {
     var_dump($payload);
+    return true;
 });
 
 $client->startWorker();
